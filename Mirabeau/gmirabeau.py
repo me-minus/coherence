ec56752d3a358357143800ed8d963dabd9d77361
philn <philn@8402a193-bf1e-0410-a7e5-8246bfde4f7d>
2010-01-24 15:21:55 +0000

    mirabeau UI: MS browser, vastly inspired from the UPnP-Inspector

--- Mirabeau/gmirabeau.py
+++ Mirabeau/gmirabeau.py
@@ -23,6 +23,7 @@ import gobject
 import gettext
 import dbus
 
+from pkg_resources import resource_filename
 
 from dbus.mainloop.glib import DBusGMainLoop
 DBusGMainLoop(set_as_default=True)
@@ -37,8 +38,11 @@ from twisted.internet import reactor
 
 from coherence.base import Coherence
 
+from coherence.upnp.core.utils import parse_xml
 from coherence.extern.simple_config import Config, XmlDictObject
 from coherence.extern.telepathy import connect
+from coherence.upnp.core import DIDLLite
+from coherence.extern.et import ET
 
 from telepathy.interfaces import ACCOUNT_MANAGER, ACCOUNT
 from telepathy.interfaces import CONN_INTERFACE
@@ -115,6 +119,8 @@ class MainWindow(hildon.StackableWindow):
         device_type = device.get_device_type().split(':')[3].lower()
         if device_type == 'mediaserver':
             print "browse MS"
+            window = MediaServerBrowser(device)
+            window.show_all()
         elif device_type == 'mediarenderer':
             print "control MR"
         else:
@@ -350,6 +356,244 @@ class DevicesView(gtk.TreeView):
     def sort_ascending(self):
         self.get_model().set_sort_column_id(self.DEVICE_NAME_COLUMN, gtk.SORT_ASCENDING)
 
+class MediaServerBrowser(hildon.StackableWindow):
+
+    def __init__(self, device):
+        super(MediaServerBrowser, self).__init__()
+        self.device = device
+        self.set_title(device.get_friendly_name())
+
+        browse_view = MediaServerBrowseView(self.device)
+        browse_view.show()
+        area = hildon.PannableArea()
+        area.add(browse_view)
+        area.show_all()
+        self.add(area)
+
+class MediaServerBrowseView(gtk.TreeView):
+
+    MS_NAME_COLUMN = 0
+    MS_NODE_ID_COLUMN = 1
+    MS_UPNP_CLASS_COLUMN = 2
+    MS_CHILD_COUNT_COLUMN = 3
+    MS_UDN_COLUMN = 4
+    MS_SERVICE_PATH_COLUMN = 5
+    MS_ICON_COLUMN = 6
+    MS_DIDL_COLUMN = 7
+    MS_TOOLTIP_COLUMN = 8
+
+    def __init__(self, device):
+        super(MediaServerBrowseView, self).__init__()
+        self.device = device
+        model = gtk.TreeStore(str, str, str, int, str, str, gtk.gdk.Pixbuf,
+                              str, gtk.gdk.Pixbuf)
+        self.set_model(model)
+        column = gtk.TreeViewColumn('Items')
+        icon_cell = gtk.CellRendererPixbuf()
+        text_cell = gtk.CellRendererText()
+
+        column.pack_start(icon_cell, False)
+        column.pack_start(text_cell, True)
+
+        column.set_attributes(text_cell, text=self.MS_NAME_COLUMN)
+        column.add_attribute(icon_cell, "pixbuf", self.MS_ICON_COLUMN)
+        self.append_column(column)
+
+        self.connect("row-expanded", self.row_expanded)
+
+        icon = resource_filename(__name__, os.path.join('icons','folder.png'))
+        self.folder_icon = gtk.gdk.pixbuf_new_from_file(icon)
+        icon = resource_filename(__name__, os.path.join('icons','audio-x-generic.png'))
+        self.audio_icon = gtk.gdk.pixbuf_new_from_file(icon)
+        icon = resource_filename(__name__, os.path.join('icons','video-x-generic.png'))
+        self.video_icon = gtk.gdk.pixbuf_new_from_file(icon)
+        icon = resource_filename(__name__, os.path.join('icons','image-x-generic.png'))
+        self.image_icon = gtk.gdk.pixbuf_new_from_file(icon)
+
+        self.load()
+
+    def load(self):
+        service = self.device.get_service_by_type('ContentDirectory')
+
+        def reply(response):
+            model = self.get_model()
+            item = model.append(None)
+            model.set_value(item, self.MS_NAME_COLUMN, 'root')
+            model.set_value(item, self.MS_NODE_ID_COLUMN, '0')
+            model.set_value(item, self.MS_UPNP_CLASS_COLUMN, 'root')
+            model.set_value(item, self.MS_CHILD_COUNT_COLUMN, -1)
+            model.set_value(item, self.MS_UDN_COLUMN, self.device.get_usn())
+            model.set_value(item, self.MS_ICON_COLUMN, self.folder_icon)
+            model.set_value(item, self.MS_DIDL_COLUMN, response['Result'])
+            model.set_value(item, self.MS_SERVICE_PATH_COLUMN, service)
+            model.set_value(item, self.MS_TOOLTIP_COLUMN, None)
+            model.append(item, ('...loading...','','placeholder',-1,'','',None,'',None))
+
+
+        action = service.get_action('Browse')
+        d = action.call(ObjectID='0',BrowseFlag='BrowseMetadata',
+                        StartingIndex=str(0),RequestedCount=str(0),
+                        Filter='*',SortCriteria='')
+        d.addCallback(reply)
+        d.addErrback(self.handle_error)
+        service.subscribe_for_variable('ContainerUpdateIDs', callback=self.state_variable_change)
+        service.subscribe_for_variable('SystemUpdateID', callback=self.state_variable_change)
+
+    def state_variable_change( self, variable):
+        name = variable.name
+        value = variable.value
+        if name == 'ContainerUpdateIDs':
+            changes = value.split(',')
+            model = self.get_model()
+
+            while len(changes) > 1:
+                container = changes.pop(0).strip()
+                update_id = changes.pop(0).strip()
+
+                def match_func(iter, data):
+                    column, key = data # data is a tuple containing column number, key
+                    value = model.get_value(iter, column)
+                    return value == key
+
+                def search(iter, func, data):
+                    while iter:
+                        if func(iter, data):
+                            return iter
+                        result = search(model.iter_children(iter), func, data)
+                        if result: return result
+                        iter = model.iter_next(iter)
+                    return None
+
+                row_count = 0
+                for row in model:
+                    iter = model.get_iter(row_count)
+                    match_iter = search(model.iter_children(iter),
+                                        match_func, (self.MS_NODE_ID_COLUMN, container))
+                    if match_iter:
+                        path = model.get_path(match_iter)
+                        expanded = self.row_expanded(path)
+                        child = model.iter_children(match_iter)
+                        while child:
+                            model.remove(child)
+                            child = model.iter_children(match_iter)
+                        self.browse(self.treeview, path, None,
+                                    starting_index=0, requested_count=0,
+                                    force=True, expand=expanded)
+
+                    break
+                    row_count += 1
+
+    def row_expanded(self, view, iter, row_path):
+        model = self.get_model()
+        child = model.iter_children(iter)
+        if child:
+            upnp_class = model.get(child, self.MS_UPNP_CLASS_COLUMN)[0]
+            if upnp_class == 'placeholder':
+                self.browse(view, row_path, None)
+
+    def browse(self, view, row_path, column, starting_index=0, requested_count=0,
+               force=False, expand=False):
+        model = self.get_model()
+        iter = model.get_iter(row_path)
+        child = model.iter_children(iter)
+        if child:
+            upnp_class = model.get(child, self.MS_UPNP_CLASS_COLUMN)[0]
+            if upnp_class != 'placeholder':
+                if force == False:
+                    if view.row_expanded(row_path):
+                        view.collapse_row(row_path)
+                    else:
+                        view.expand_row(row_path, False)
+                    return
+
+        title, object_id, upnp_class = model.get(iter, self.MS_NAME_COLUMN,
+                                                 self.MS_NODE_ID_COLUMN,
+                                                 self.MS_UPNP_CLASS_COLUMN)
+        if (not upnp_class.startswith('object.container') and
+            not upnp_class == 'root'):
+            url = model.get(iter, self.MS_SERVICE_PATH_COLUMN)[0]
+            if url == '':
+                return
+            print "request to play:", title,object_id,url
+            # TODO: fire up the predefined MR
+            return
+
+        def reply(r):
+            child = model.iter_children(iter)
+            if child:
+                upnp_class = model.get(child, self.MS_UPNP_CLASS_COLUMN)[0]
+                if upnp_class == 'placeholder':
+                    model.remove(child)
+
+            title = model.get(iter, self.MS_NAME_COLUMN)[0]
+            try:
+                title = title[:title.rindex('(')]
+                model.set_value(iter, self.MS_NAME_COLUMN,
+                                "%s(%d)" % (title, int(r['TotalMatches'])))
+            except ValueError:
+                pass
+            elt = parse_xml(r['Result'], 'utf-8')
+            elt = elt.getroot()
+            for child in elt:
+                stored_didl_string = DIDLLite.element_to_didl(ET.tostring(child))
+                didl = DIDLLite.DIDLElement.fromString(stored_didl_string)
+                item = didl.getItems()[0]
+                if item.upnp_class.startswith('object.container'):
+                    icon = self.folder_icon
+                    service = model.get(iter, self.MS_SERVICE_PATH_COLUMN)[0]
+                    child_count = item.childCount
+                    try:
+                        title = "%s (%d)" % (item.title, item.childCount)
+                    except TypeError:
+                        title = "%s (n/a)" % item.title
+                        child_count = -1
+                else:
+                    icon = None
+                    service = ''
+
+                    child_count = -1
+                    title = item.title
+                    if item.upnp_class.startswith('object.item.audioItem'):
+                        icon = self.audio_icon
+                    elif item.upnp_class.startswith('object.item.videoItem'):
+                        icon = self.video_icon
+                    elif item.upnp_class.startswith('object.item.imageItem'):
+                        icon = self.image_icon
+
+                    res = item.res.get_matching(['*:*:*:*'], protocol_type='http-get')
+                    if len(res) > 0:
+                        res = res[0]
+                        service = res.data
+
+                new_iter = model.append(iter, (title, item.id, item.upnp_class, child_count,
+                                               '',service,icon,stored_didl_string,None))
+                if item.upnp_class.startswith('object.container'):
+                    model.append(new_iter, ('...loading...',
+                                            '', 'placeholder', -1, '', '',
+                                            None, '', None))
+
+
+            if ((int(r['TotalMatches']) > 0 and force==False) or \
+                expand==True):
+                view.expand_row(row_path, False)
+
+            if(requested_count != int(r['NumberReturned']) and \
+               int(r['NumberReturned']) < (int(r['TotalMatches'])-starting_index)):
+                self.browse(view, row_path, column,
+                            starting_index=starting_index+int(r['NumberReturned']),
+                            force=True)
+
+        service = self.device.get_service_by_type('ContentDirectory')
+        action = service.get_action('Browse')
+        d = action.call(ObjectID=object_id,BrowseFlag='BrowseDirectChildren',
+                        StartingIndex=str(starting_index),RequestedCount=str(requested_count),
+                        Filter='*',SortCriteria='')
+        d.addCallback(reply)
+        d.addErrback(self.handle_error)
+        return d
+
+    def handle_error(self,error):
+        print error
 
 class SettingsDialog(gtk.Dialog):
 
