693e195216054f53f511ada3042378e67fbb0e75
Hartmut Goebel <h.goebel@goebel-consult.de>
2011-06-13 22:22:34 +0200

    Enhanced setup.py: long description (from README), classifiers, and minor changes.

--- Cadre/setup.py
+++ Cadre/setup.py
@@ -6,19 +6,30 @@ from cadre import __version__
 
 packages = find_packages()
 
+long_description = "\n\n".join([
+    open("README.txt").read(),
+    ])
+
 setup(
     name="Cadre",
     version=__version__,
-    description="""Cadre - a PictureFrame application based on the Coherence DLNA/UPnP framework""",
+    description="""A DLNA/UPnP-PictureFrame application based on the Coherence framework""",
+    long_description = long_description,
     author="Frank Scholz",
     author_email='fs@beebits.net',
+    license = "MIT",
     packages=packages,
     include_package_data = True,
     zipfile = None,
     scripts = ['bin/cadre'],
     url = "http://coherence-project.org",
-
-    package_data = {
-        'cadre': []
-    },
-)
+    classifiers = [
+        #'Development Status :: 5 - Production/Stable',
+        #'Environment :: Console',
+        #'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        ]
+    )
